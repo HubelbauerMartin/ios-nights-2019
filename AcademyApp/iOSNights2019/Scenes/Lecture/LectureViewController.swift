6b085611d768a4ae42dced1001757bc6e81a7249
Jakub Vodak <jakub.vodak@strv.com>
2019-04-04 13:43:11 +0200

    Lecture detail UI

--- AcademyApp/iOSNights2019/Scenes/Lecture/LectureViewController.swift
+++ AcademyApp/iOSNights2019/Scenes/Lecture/LectureViewController.swift
@@ -9,9 +9,39 @@
 import UIKit
 
 class LectureViewController: UIViewController {
+    // MARK: - Outlets content
+
+    @IBOutlet var imgHeader: UIImageView!
+    @IBOutlet var lblTitle: UILabel!
+    @IBOutlet var txtContent: UITextView!
+
+    // MARK: - Outlets control
+
+    @IBOutlet var contentControl: UISegmentedControl!
+    @IBOutlet var attendedSwitch: UISwitch!
+
+    // MARK: - Outlets constraints
+
+    @IBOutlet var shadownHeightConstraint: NSLayoutConstraint!
+
+    // MARK: - Object lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupUI()
     }
+}
+
+private extension LectureViewController {
+    // MARK: - Appearance
+
+    func setupUI() {
+        contentControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: .selected)
+        txtContent.textContainerInset = UIEdgeInsets(top: 20, left: 0, bottom: shadownHeightConstraint.constant, right: 0)
+    }
+
+    // MARK: - Action
+    @IBAction func switchAction(_: UISwitch) {}
+
+    @IBAction func segmentAction(_: UISegmentedControl) {}
 }
